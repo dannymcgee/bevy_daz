e2f4821c31b6f336ab2cc43e2846fe8be77b6b2b
Danny McGee <dannymcgee@gmail.com>
2024-04-17 13:02:46 -0400

    Create a separate crate for DQS implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 use bevy::app::{PluginGroup, PluginGroupBuilder};
 
 mod asset;
-mod dqs;
 mod io;
 mod runtime;
 mod spawning;
 
 pub use crate::{
 	asset::{DazAsset, DazAssetTypesPlugin, DazMesh, DazNode, DazPrimitive, DazUvSet},
-	dqs::DualQuat,
 	io::{DazAssetReader, DazAssetSourcePlugin},
 	runtime::DazRuntimePlugin,
 	spawning::{DazBone, DazFigure, DazSpawningPlugin},
 };
+pub use bevy_dqskinning::DualQuat;
 pub use daz_asset_types::NodeType;
 
 pub struct DazPlugins;
